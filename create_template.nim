546ecc35ec47625c604f14a8d692c4562db6b83b
LiberaTeMetuMortis <78408599+LiberaTeMetuMortis@users.noreply.github.com>
2022-12-05 23:26:05 +0300

    fix: Remove debug prints fix: Wrap file operations with try except block

--- create_template.nim
+++ create_template.nim
@@ -8,23 +8,59 @@ stdout.write("Enter your project's group ID: ")
 let groupID = readLine(stdin)
 stdout.write("Enter your artifact ID: ")
 let artifactID = readLine(stdin)
+
+# Get zip buffer from GitHub.
 let client = newHttpClient()
 let content = client.getContent("https://github.com/LiberaTeMetuMortis/KotlinSpigotPluginTemplate/zipball/raw")
-writeFile("content.zip", content)
-extractAll("content.zip", "unzipped")
-removeFile("content.zip")
+
+# Write zip buffer to file.
+try:
+  writeFile("content.zip", content)
+except:
+  echo "Failed to write zip buffer to content.zip"
+  quit(1)
+
+# Extract zip file.
+try:
+  extractAll("content.zip", "unzipped")
+except:
+  echo "Failed to extract zip file content.zip to unzipped"
+  quit(1)
+
+# Remove zip file.
+try:
+  removeFile("content.zip")
+except:
+  echo "Failed to remove content.zip"
+  quit(1)
+
+# Find Template directory.
 for file in walkDir("./unzipped"):
-  echo file.path
-  if match(file.path, peg"unzipped[\/\\]LiberaTeMetuMortis\-KotlinSpigotPluginTemplate\-.+"):
-    echo "Dosya bulundu."
+  if match(file.path, peg"unzipped\/LiberaTeMetuMortis\-KotlinSpigotPluginTemplate\-.+"):
+    # Move Template directory to the main folder.
     try:
       moveDir(file.path, artifactID)
     except OSError:
       echo "You already have a directory named ", artifactID
-removeDir("unzipped")
+      quit(1)
+
+# Remove unzipped folder.
+try:
+  removeDir("unzipped")
+except:
+  echo "Couldn't remove unzipped directory."
+  quit(1)
+
+# Create project's group directory.
 let replacedGroupID = replace(groupID, '.', '/')
 let projectDir = artifactID&"/project/src/main/kotlin/"&replacedGroupID
-createDir(projectDir)
+try:
+  createDir(projectDir)
+except:
+  echo "Couldn't create "&projectDir
+  quit(1)
+
+# Create main file of the project.
 let contentOfMain = "package "&groupID&"""
 
 
@@ -40,11 +76,26 @@ class Main : JavaPlugin() {
     }
 }
 """
-writeFile(projectDir&"/"&artifactID&".kt", contentOfMain)
+
+try: 
+  writeFile(projectDir&"/"&artifactID&".kt", contentOfMain)
+except: 
+  echo "Couldn't write into "&projectDir&"/"&artifactID&".kt"
+  quit(1)
+
+# Write artifact and group ID into build.gradle.kts.
 var pluginConfig: File
 if open(pluginConfig, artifactID&"/project/src/main/resources/plugin.yml", FileMode.fmAppend):
   writeLine(pluginConfig, "name: \""&artifactID&"\"")
-  writeLine(pluginConfig, "main: \""&groupID&"."&artifactID&"\"")
+  writeLine(pluginConfig, "main: \""&groupID&artifactID&"\"")
+else:
+  echo "Couldn't open plugin.yml"
+  quit(1)
+
+# Write artifact ID and root project name into settings.gradle.kts.
 var gradleConfig: File
 if open(gradleConfig, artifactID&"/settings.gradle.kts", FileMode.fmAppend):
   writeLine(gradleConfig, "rootProject.name = "&"\""&artifactID&"\"")
+else:
+  echo "Couldn't open settings.gradle.kts"
+  quit(1)
