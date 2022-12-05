55d12a39807b921fdc2ca35d3ed082aee6e7e2fa
LiberaTeMetuMortis <78408599+LiberaTeMetuMortis@users.noreply.github.com>
2022-12-06 00:04:42 +0300

    feat: Add colorful debug messages

--- create_template.nim
+++ create_template.nim
@@ -1,6 +1,6 @@
 import std/httpclient
 import zippy/ziparchives
-import std/[os,pegs,strutils]
+import std/[os,pegs,strutils,terminal]
 proc ctrlc() {.noconv.} =
   quit(0)
 setControlCHook(ctrlc)
@@ -12,26 +12,36 @@ let artifactID = readLine(stdin)
 # Get zip buffer from GitHub.
 let client = newHttpClient()
 let content = client.getContent("https://github.com/LiberaTeMetuMortis/KotlinSpigotPluginTemplate/zipball/raw")
+stdout.styledWriteLine(fgGreen, "Downloaded zip file from GitHub.")
 
 # Write zip buffer to file.
 try:
   writeFile("content.zip", content)
+  stdout.styledWriteLine(fgGreen, "Wrote zip file to content.zip.")
+  stdout.resetAttributes()
 except:
-  echo "Failed to write zip buffer to content.zip"
+  stdout.styledWriteLine(fgRed, "Failed to write zip buffer to content.zip.")
+  stdout.resetAttributes()
   quit(1)
 
 # Extract zip file.
 try:
   extractAll("content.zip", "unzipped")
+  stdout.styledWriteLine(fgGreen, "Extracted content.zip to unzipped directory.")
+  stdout.resetAttributes()
 except:
-  echo "Failed to extract zip file content.zip to unzipped"
+  stdout.styledWriteLine(fgRed, "Failed to extract content.zip to unzipped.")
+  stdout.resetAttributes()
   quit(1)
 
 # Remove zip file.
 try:
   removeFile("content.zip")
+  stdout.styledWriteLine(fgGreen, "Removed content.zip file.")
+  stdout.resetAttributes()
 except:
-  echo "Failed to remove content.zip"
+  stdout.styledWriteLine(fgRed, "Failed to remove content.zip.")
+  stdout.resetAttributes()
   quit(1)
 
 # Find Template directory.
@@ -40,15 +50,21 @@ for file in walkDir("./unzipped"):
     # Move Template directory to the main folder.
     try:
       moveDir(file.path, artifactID)
+      stdout.styledWriteLine(fgGreen, "Moved Template directory to the main folder.")
+      stdout.resetAttributes()
     except OSError:
-      echo "You already have a directory named ", artifactID
+      stdout.styledWriteLine(fgRed, "You already have a directory named "&artifactID&".")
+      stdout.resetAttributes()
       quit(1)
 
 # Remove unzipped folder.
 try:
   removeDir("unzipped")
+  stdout.styledWriteLine(fgGreen, "Removed unzipped folder.")
+  stdout.resetAttributes()
 except:
-  echo "Couldn't remove unzipped directory."
+  stdout.styledWriteLine(fgRed, "Couldn't remove unzipped directory.")
+  stdout.resetAttributes()
   quit(1)
 
 # Create project's group directory.
@@ -56,8 +72,11 @@ let replacedGroupID = replace(groupID, '.', '/')
 let projectDir = artifactID&"/project/src/main/kotlin/"&replacedGroupID
 try:
   createDir(projectDir)
+  stdout.styledWriteLine(fgGreen, "Created project's group directory.")
+  stdout.resetAttributes()
 except:
-  echo "Couldn't create "&projectDir
+  stdout.styledWriteLine(fgRed, "Couldn't create "&projectDir&" directory.")
+  stdout.resetAttributes()
   quit(1)
 
 # Create main file of the project.
@@ -79,8 +98,11 @@ class Main : JavaPlugin() {
 
 try: 
   writeFile(projectDir&"/"&artifactID&".kt", contentOfMain)
+  stdout.styledWriteLine(fgGreen, "Created main file of the project.")
+  stdout.resetAttributes()
 except: 
-  echo "Couldn't write into "&projectDir&"/"&artifactID&".kt"
+  stdout.styledWriteLine(fgRed, "Couldn't write into "&projectDir&"/"&artifactID&".kt.")
+  stdout.resetAttributes()
   quit(1)
 
 # Write artifact and group ID into build.gradle.kts.
@@ -88,14 +110,20 @@ var pluginConfig: File
 if open(pluginConfig, artifactID&"/project/src/main/resources/plugin.yml", FileMode.fmAppend):
   writeLine(pluginConfig, "name: \""&artifactID&"\"")
   writeLine(pluginConfig, "main: \""&groupID&artifactID&"\"")
+  stdout.styledWriteLine(fgGreen, "Wrote artifact and group ID into plugin.yml.")
+  stdout.resetAttributes()
 else:
-  echo "Couldn't open plugin.yml"
+  stdout.styledWriteLine(fgRed, "Couldn't open plugin.yml.")
+  stdout.resetAttributes()
   quit(1)
 
 # Write artifact ID and root project name into settings.gradle.kts.
 var gradleConfig: File
 if open(gradleConfig, artifactID&"/settings.gradle.kts", FileMode.fmAppend):
   writeLine(gradleConfig, "rootProject.name = "&"\""&artifactID&"\"")
+  stdout.styledWriteLine(fgGreen, "Wrote artifact ID and root project name into settings.gradle.kts.")
+  stdout.resetAttributes()
 else:
-  echo "Couldn't open settings.gradle.kts"
+  stdout.styledWriteLine(fgRed, "Couldn't open settings.gradle.kts.")
+  stdout.resetAttributes()
   quit(1)
