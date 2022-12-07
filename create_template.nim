16ea32f88f7f708b4e83dc4e95cf7159e9de3924
LiberaTeMetuMortis <78408599+LiberaTeMetuMortis@users.noreply.github.com>
2022-12-08 01:06:54 +0300

    fix: Removed unnecessary resetAttributes

--- create_template.nim
+++ create_template.nim
@@ -18,30 +18,24 @@ stdout.styledWriteLine(fgGreen, "Downloaded zip file from GitHub.")
 try:
   writeFile("content.zip", content)
   stdout.styledWriteLine(fgGreen, "Wrote zip file to content.zip.")
-  stdout.resetAttributes()
 except:
   stdout.styledWriteLine(fgRed, "Failed to write zip buffer to content.zip.")
-  stdout.resetAttributes()
   quit(1)
 
 # Extract zip file.
 try:
   extractAll("content.zip", "unzipped")
   stdout.styledWriteLine(fgGreen, "Extracted content.zip to unzipped directory.")
-  stdout.resetAttributes()
 except:
   stdout.styledWriteLine(fgRed, "Failed to extract content.zip to unzipped.")
-  stdout.resetAttributes()
   quit(1)
 
 # Remove zip file.
 try:
   removeFile("content.zip")
   stdout.styledWriteLine(fgGreen, "Removed content.zip file.")
-  stdout.resetAttributes()
 except:
   stdout.styledWriteLine(fgRed, "Failed to remove content.zip.")
-  stdout.resetAttributes()
   quit(1)
 
 # Find Template directory.
@@ -51,20 +45,18 @@ for file in walkDir("./unzipped"):
     try:
       moveDir(file.path, artifactID)
       stdout.styledWriteLine(fgGreen, "Moved Template directory to the main folder.")
-      stdout.resetAttributes()
+  
     except OSError:
       stdout.styledWriteLine(fgRed, "You already have a directory named "&artifactID&".")
-      stdout.resetAttributes()
+  
       quit(1)
 
 # Remove unzipped folder.
 try:
   removeDir("unzipped")
   stdout.styledWriteLine(fgGreen, "Removed unzipped folder.")
-  stdout.resetAttributes()
 except:
   stdout.styledWriteLine(fgRed, "Couldn't remove unzipped directory.")
-  stdout.resetAttributes()
   quit(1)
 
 # Create project's group directory.
@@ -73,10 +65,8 @@ let projectDir = artifactID&"/project/src/main/kotlin/"&replacedGroupID
 try:
   createDir(projectDir)
   stdout.styledWriteLine(fgGreen, "Created project's group directory.")
-  stdout.resetAttributes()
 except:
   stdout.styledWriteLine(fgRed, "Couldn't create "&projectDir&" directory.")
-  stdout.resetAttributes()
   quit(1)
 
 # Create main file of the project.
@@ -99,10 +89,8 @@ class Main : JavaPlugin() {
 try: 
   writeFile(projectDir&"/"&artifactID&".kt", contentOfMain)
   stdout.styledWriteLine(fgGreen, "Created main file of the project.")
-  stdout.resetAttributes()
 except: 
   stdout.styledWriteLine(fgRed, "Couldn't write into "&projectDir&"/"&artifactID&".kt.")
-  stdout.resetAttributes()
   quit(1)
 
 # Write artifact and group ID into build.gradle.kts.
@@ -111,10 +99,8 @@ if open(pluginConfig, artifactID&"/project/src/main/resources/plugin.yml", FileM
   writeLine(pluginConfig, "name: \""&artifactID&"\"")
   writeLine(pluginConfig, "main: \""&groupID&artifactID&"\"")
   stdout.styledWriteLine(fgGreen, "Wrote artifact and group ID into plugin.yml.")
-  stdout.resetAttributes()
 else:
   stdout.styledWriteLine(fgRed, "Couldn't open plugin.yml.")
-  stdout.resetAttributes()
   quit(1)
 
 # Write artifact ID and root project name into settings.gradle.kts.
@@ -122,8 +108,6 @@ var gradleConfig: File
 if open(gradleConfig, artifactID&"/settings.gradle.kts", FileMode.fmAppend):
   writeLine(gradleConfig, "rootProject.name = "&"\""&artifactID&"\"")
   stdout.styledWriteLine(fgGreen, "Wrote artifact ID and root project name into settings.gradle.kts.")
-  stdout.resetAttributes()
 else:
   stdout.styledWriteLine(fgRed, "Couldn't open settings.gradle.kts.")
-  stdout.resetAttributes()
   quit(1)
