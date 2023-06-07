e471680b5c3f084dfc95cb03af1b52add3978d25
MetuMortis <78408599+LiberaTeMetuMortis@users.noreply.github.com>
2023-06-08 00:15:25 +0300

    fix: Use OS spesific path seperator instead of "/"

--- script.gradle.kts
+++ script.gradle.kts
@@ -1,6 +1,6 @@
 open class MultiVersionPlugin : DefaultTask() {
     override fun getGroup() =  "application"
-    override fun getDescription() = "Creates copies of project for diffrent spigot versions"
+    override fun getDescription() = "Creates copies of project for diffrent Spigot versions"
 
     @Optional
     @get:Input
@@ -12,6 +12,7 @@ open class MultiVersionPlugin : DefaultTask() {
 
     @TaskAction
     fun create() {
+        val seperator = File.separator
         val dir = project.projectDir
         val rootDir = project.rootDir
         if(versions == null) versions = "all"
@@ -26,13 +27,13 @@ open class MultiVersionPlugin : DefaultTask() {
             //createVersionFolder(version)
             for(file in dir.listFiles()!!){
                 if(file.name != "build.gradle.kts" && file.name != "build"){
-                    val newFile = File("${rootDir}/project-${version}/${file.name}")
+                    val newFile = File("${rootDir}${seperator}project-${version}${seperator}${file.name}")
                     if(!newFile.exists()){
                         file.copyRecursively(newFile)
                         if(file.isDirectory && file.name == "src") {
-                            val pluginFile = File(file.absolutePath + "/main/resources/plugin.yml")
+                            val pluginFile = File(file.absolutePath + "${seperator}main${seperator}resources${seperator}plugin.yml")
                             if(pluginFile.exists()){
-                                val newPluginFile = File("${rootDir}/project-${version}/${file.name}/main/resources/plugin.yml")
+                                val newPluginFile = File("${rootDir}${seperator}project-${version}${seperator}${file.name}${seperator}main${seperator}resources${seperator}plugin.yml")
                                 val content = pluginFile.readText().replace(Regex("(api-version: +)[\"'][0-9.]+[\"']"), "$1\"${version}\"")
                                 newPluginFile.writeText(content)
                             }
@@ -42,7 +43,7 @@ open class MultiVersionPlugin : DefaultTask() {
                 }
 
                 else if(file.name == "build.gradle.kts"){
-                    val newFile = File("${rootDir}/project-${version}/${file.name}")
+                    val newFile = File("${rootDir}${seperator}project-${version}${seperator}${file.name}")
                     if(!newFile.exists()){
                         file.copyRecursively(newFile)
                     }
@@ -70,19 +71,13 @@ open class MultiVersionPlugin : DefaultTask() {
                     */
                     newFile.writeText(newContent)
                 }
-
-
-            }
-            val mainSettingFile = File(rootDir.absolutePath+"/settings.gradle.kts")
-            if(mainSettingFile.readLines().all { !it.contains("include(\"project-${version}\")") }){
-                mainSettingFile.appendText("\ninclude(\"project-${version}\")")
             }
         }
     }
 
     fun createVersionFolder(version: String){
         val rootDir = project.rootDir
-        val folder = File(rootDir.path+"/project-$version")
+        val folder = File(rootDir.path,"project-$version")
         if(!folder.exists()) folder.mkdirs()
     }
 }
@@ -93,14 +88,14 @@ open class moveBuilds : DefaultTask() {
     override fun getDescription() = "Moves all builds from all versions of project to root project's build folder"
     @TaskAction
     fun move(){
+        val seperator = File.separator
         val rootDir = project.rootDir
         project.projectDir
-        if(!File(rootDir.absolutePath+"/builds").exists()) File(rootDir.absolutePath+"/builds").mkdirs()
+        if(!File(rootDir.absolutePath,"builds").exists()) File(rootDir.absolutePath,"builds").mkdirs()
         rootDir.listFiles()!!.filter { it.name.matches(Regex("project-[0-9.]+")) }.forEach {
             val version = it.name.replace("project-", "")
-            println(rootDir.absolutePath+"/builds")
             try{
-                File(it.absolutePath+"/build/libs/").listFiles()?.find { x -> x.name.contains("-all.jar") }!!.copyTo(File(rootDir.absolutePath+"/build/$version.jar"))
+                File(it.absolutePath+"${seperator}build${seperator}libs${seperator}").listFiles()?.find { x -> x.name.contains("-all.jar") }!!.copyTo(File(rootDir.absolutePath+"${seperator}build${seperator}$version.jar"))
             }catch (ignored: Exception){}
         }
     }
